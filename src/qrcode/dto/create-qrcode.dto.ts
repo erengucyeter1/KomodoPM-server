778ab4567f90f76ff21c513fd2402624e22abfef
erengucyeter1 <erengucyeter0@gmail.com>
2025-05-03 12:52:21 +0300

    feat(qrcode): update CreateQrcodeDto structure and add logo as base64

--- src/qrcode/dto/create-qrcode.dto.ts
+++ src/qrcode/dto/create-qrcode.dto.ts
@@ -4,7 +4,11 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateQrcodeDto {
     @IsNotEmpty()
     @IsString()
-    data: string;
+    code: string;
+
+    @IsNotEmpty()
+    @IsString()
+    mesurement: string;
 
 }
 
