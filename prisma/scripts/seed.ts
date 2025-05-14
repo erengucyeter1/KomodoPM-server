f4d896f15f14a7e3362aabf466e05aa13a395434
erengucyeter1 <erengucyeter0@gmail.com>
2025-05-14 23:30:37 +0300

    seeds has been updated in order

--- prisma/scripts/seed.ts
+++ prisma/scripts/seed.ts
@@ -4,79 +4,94 @@ import * as bcrypt from 'bcrypt'
 const prisma = new PrismaClient()
 
 async function main() {
-  // Tablolarda veri var mı kontrol et
   const userCount = await prisma.user.count()
-  const permissionCount = await prisma.permissions.count()
+  const permissionCount = await prisma.permission.count()
   const roleCount = await prisma.role.count()
 
-  // İzinleri ekle (eğer yoksa)
+  let adminPermission
+  let adminRole
+  let  adminUser
+  // Permission ekle
   if (permissionCount === 0) {
     console.log('Permissions tablosuna veriler ekleniyor...')
-    
-    const adminPermission = await prisma.permissions.create({
+
+    adminPermission = await prisma.permission.create({
       data: {
         name: 'admin',
-        description: 'Tüm admin paneline erişim sağlar'
-      }
+        description: 'Tüm admin paneline erişim sağlar',
+      },
     })
-    
+
     console.log('Admin permission oluşturuldu:', adminPermission)
+  } else {
+    adminPermission = await prisma.permission.findUnique({
+      where: { name: 'admin' },
+    })
   }
 
-  // Rolleri ekle (eğer yoksa)
+  // Role ekle
   if (roleCount === 0) {
-    console.log('Role tablosuna veriler ekleniyor...')
-    
-    // Önce izinleri al
-    const adminPermission = await prisma.permissions.findFirst({
-      where: { name: 'admin' }
-    })
+    console.log('Roles tablosuna veriler ekleniyor...')
 
     if (adminPermission) {
-      const adminRole = await prisma.role.create({
+      adminRole = await prisma.role.create({
         data: {
           name: 'admin',
           description: 'Sistem yöneticisi',
-          // İzinleri bir dizi olarak ekle
-          permissions: [adminPermission.id]
-        }
+          permissions: {
+            connect: [{ id: adminPermission.id }],
+          },
+        },
       })
-      
-      console.log('Admin role oluşturuldu:', adminRole)
+
+      console.log('Admin rolü oluşturuldu:', adminRole)
     }
+  } else {
+    adminRole = await prisma.role.findUnique({
+      where: { name: 'admin' },
+      include: { permissions: true },
+    })
   }
 
-  // Admin kullanıcısı ekle (eğer yoksa)
+  // Admin kullanıcıyı ekle
   if (userCount === 0) {
     console.log('User tablosuna admin kullanıcısı ekleniyor...')
-    
-    // Admin rolünü bul
-    const adminRole = await prisma.role.findFirst({
-      where: { name: 'admin' }
-    })
 
     if (adminRole) {
-      // Şifreyi hash'le
       const hashedPassword = await bcrypt.hash('0000Eren!15975321', 10)
-      
-      const adminUser = await prisma.user.create({
-        data: {
-          username: 'admin',
-          name: 'Admin',         
-          surname: 'User',       
-          email: 'admin@example.com',
-          password: hashedPassword,
-          // Eksik olan authorization_rank alanını ekle - en yüksek yetki için 100 değeri verelim
-          authorization_rank: 9,
-          // Roller bir dizi olarak ekleniyor
-          roles: [Number(adminRole.id)],
-          permissions: adminRole.permissions // İzinleri de ekle,
-        }
-      })
-      
-      console.log('Admin kullanıcısı oluşturuldu:', adminUser)
+
+      try{
+        adminUser = await prisma.user.create({
+          data: {
+            username: 'admin',
+            name: 'Admin',
+            surname: 'User',
+            email: 'admin@example.com',
+            password: hashedPassword,
+            authorization_rank: 9,
+            roles: {
+              connect: [{ id: adminRole.id }],
+            },
+          },
+          include: {
+            roles: true,
+          },
+        })
+
+        console.log('Admin kullanıcısı oluşturuldu:', adminUser)
+
+
+      }catch (error){
+        console.log(error)
+      }
+     
+
     }
+  } else {
+    console.log('User tablosunda kullanıcı bulundu, eklenmedi.')
   }
+
+  console.log('Seed işlemi tamamlandı.')
 }
 
 main()
@@ -84,7 +99,7 @@ main()
     await prisma.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e)
+    console.error('Seed işlemi sırasında hata:', e)
     await prisma.$disconnect()
     process.exit(1)
   })
